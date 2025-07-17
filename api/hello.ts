8c620ff0cec83ad45f54991f75bbe1434026b3eb
H1-N1 <erwibi25@gmail.com>
2025-07-17 13:10:41 +0100

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -2,7 +2,48 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { app } from 'google-play-scraper';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const { package: pkg } = req.query;
+    const { package: pkg, m } = req.query;
+
+    if (!pkg || Array.isArray(pkg)) {
+        return res.status(400).json({ error: 'Missing or invalid ?package=com.example.app' });
+    }
+
+    try {
+        const data = await app({ appId: pkg });
+
+        const timestamp = data.updatedTimestamp || data.updated;
+        const lastUpdate = timestamp
+            ? new Date(timestamp).toLocaleDateString('en-GB', {
+                day: '2-digit',
+                month: 'short',
+                year: 'numeric'
+            }).replace(/ /g, ' ')
+            : null;
+
+        if (m === 'all') {
+            return res.status(200).json(data);
+        }
+
+        return res.status(200).json({
+            package: data.appId,
+            name: data.title,
+            icon: data.icon,
+            version: data.version,
+            installs: data.installs,
+            rating: parseFloat(data.score).toFixed(1),
+            developer: data.developer,
+            lastUpdate: lastUpdate
+        });
+
+    } catch (err) {
+        return res.status(500).json({ error: 'Failed to fetch app info.', details: err.message });
+    }
+
+}
+
+
+/*
+const { package: pkg } = req.query;
 
   if (!pkg || Array.isArray(pkg)) {
     return res.status(400).json({ error: 'Missing or invalid ?package=com.example.app' });
@@ -21,11 +62,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       }).replace(/ /g, ' ')
     : null;
 
-
-    // Round rating to 1 decimal
-    const rating = (typeof data.score === 'number' ? Math.round(data.score * 10) / 10 :
-                   typeof data.rating === 'number' ? Math.round(data.rating * 10) / 10 :
-                   null);
     //return res.status(200).json(data);
     return res.status(200).json({
         package: data.appId,
@@ -42,11 +78,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   } catch (err: any) {
     return res.status(500).json({ error: 'Failed to fetch app info.', details: err.message });
   }
-}
-
-
-/*
-
 
 
 */
